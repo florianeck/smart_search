2380d2defaa1e2963a8dd4115550ea5bd8759a11
Florian Eck <florian.eck@el-digital.de>
2023-08-07 06:59:44 +0200

    fix dep

--- lib/smart_search/engine.rb
+++ lib/smart_search/engine.rb
@@ -2,10 +2,9 @@ module SmartSearch
   class Engine < Rails::Engine
 
     engine_name 'smart_search'
-    
+
     isolate_namespace SmartSearch
     require "friendly_extensions"
-    require "amatch"
 
   end
 end
